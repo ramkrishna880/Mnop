04d8386c2459d4700d8fda15701d56b88086310c
ramkrishna880 <rampgroupsurface@gmail.com>
2016-03-22 19:00:09 +0530

    custom nav bar added

---
+++ Lapanzo/ORNavigationBar.m
@@ -0,0 +1,95 @@
+//
+//  ORNavigationBar.m
+//  BIGEO
+//
+//  Created by PTG on 08/03/16.
+//  Copyright © 2016 People Tech Group. All rights reserved.
+//
+
+#import "ORNavigationBar.h"
+
+const CGFloat VFSNavigationBarHeightIncrease = 38.f;
+#define NAVIGATION_BTN_MARGIN 5
+
+@implementation ORNavigationBar
+
+
+- (CGSize)sizeThatFits:(CGSize)size {
+    
+    CGSize amendedSize = [super sizeThatFits:size];
+    amendedSize.height += VFSNavigationBarHeightIncrease;
+    return amendedSize;
+}
+
+
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    
+    NSArray *classNamesToReposition = @[@"_UINavigationBarBackground"];
+    
+    
+    UINavigationItem *navigationItem = [self topItem];
+    
+    UIView *subview = [[navigationItem rightBarButtonItem] customView];
+    
+    if (subview) {
+        CGRect subviewFrame = subview.frame;
+        subviewFrame.origin.x = self.frame.size.width - subviewFrame.size.width;
+        
+        [subview setFrame:subviewFrame];
+    }
+    
+    
+    for (UIView *view in [self subviews]) {
+        
+        if ([classNamesToReposition containsObject:NSStringFromClass([view class])]) {
+            
+            CGRect bounds = [self bounds];
+            CGRect frame = [view frame];
+            frame.origin.y = bounds.origin.y + VFSNavigationBarHeightIncrease - 20.f;
+            frame.size.height = bounds.size.height + 20.f;
+            
+            [view setFrame:frame];
+        }
+    }
+}
+
+
+- (id)initWithCoder:(NSCoder *)aDecoder {
+    
+    self = [super initWithCoder:aDecoder];
+    
+    if (self) {
+        [self initialize];
+    }
+    
+    return self;
+}
+
+- (id)initWithFrame:(CGRect)frame {
+    
+    self = [super initWithFrame:frame];
+    
+    if (self) {
+        [self initialize];
+    }
+    
+    return self;
+}
+
+- (void)initialize {
+    
+    [self setTransform:CGAffineTransformMakeTranslation(0, -(VFSNavigationBarHeightIncrease))];
+}
+
+
+
+/*
+ // Only override drawRect: if you perform custom drawing.
+ // An empty implementation adversely affects performance during animation.
+ - (void)drawRect:(CGRect)rect {
+ // Drawing code
+ }
+ */
+
+@end
