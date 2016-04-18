dffc5c24f161616c9a314c7e1f39d104146403df
sampath kumar rao <sampath@sampaths-macbook-pro.local>
2016-04-18 21:09:13 +0530

    location alert added

--- Lapanzo/CategoriesVC.m
+++ Lapanzo/CategoriesVC.m
@@ -172,6 +172,11 @@
     LMAlertView *cardAlertView = [[LMAlertView alloc] initWithTitle:@"Choose a card" message:nil delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"Done",nil];
     
     [cardAlertView setSize:CGSizeMake(270.0, 167.0)];
+    UIView *contentView = cardAlertView.contentView;
+    UIViewController *vcPopUp = INSTANTIATE(@"searchid");
+    [self addChildViewController:vcPopUp];
+    [vcPopUp didMoveToParentViewController:self];
+    [contentView addSubview:vcPopUp.view];
     [cardAlertView show];
 }
 
