1a30f4fd7ddd02775e6d8d764ffd95c5604bc2e9
ramkrishna880 <ramkrishna880@gmail.com>
2016-06-01 18:40:34 +0530

    new additions

---
+++ Lapanzo/ForgotPwdVC.h
@@ -0,0 +1,14 @@
+//
+//  ForgotPwdVC.h
+//  Lapanzo
+//
+//  Created by PTG on 01/06/16.
+//  Copyright © 2016 People Tech Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "GenericViewController.h"
+
+@interface ForgotPwdVC : GenericViewController
+
+@end
